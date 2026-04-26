dcf085da829c431a1e051b8fe517f2d79b97b5ee
Encheng Xie <89811861+XYgugugu@users.noreply.github.com>
2026-04-26 18:11:32 -0500

    Implement Loss Timer to GameManager

--- Assets/PVZ/Core/GameManager.cs
+++ Assets/PVZ/Core/GameManager.cs
@@ -1,12 +1,76 @@
 using PVZ3D.Plants;
 using PVZ3D.Zombies;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using System.Collections;
 
 namespace PVZ3D.Core
 {
-    // public class GameManager : MonoBehaviour
-    // {
-        
-    // }
+    public class GameManager : MonoBehaviour
+    {
+        [Header("Loss Timer")]
+        [SerializeField] private LossTimer lossTimer = new LossTimer();
+
+        public LossTimer LossTimer => lossTimer;
+
+        private void Awake()
+        {
+            lossTimer.StartTimer(10f);
+        }
+
+        private void Update()
+        {
+            lossTimer.Update(Time.deltaTime);
+        }
+    }
+
+    [System.Serializable]
+    public class LossTimer
+    {
+        [SerializeField] private float timeRemain;
+
+        private bool isRunning;
+        private bool isPaused;
+
+        public float TimeRemain => timeRemain;
+        public bool IsRunning => isRunning;
+        public bool IsPaused => isPaused;
+
+        public void StartTimer(float duration)
+        {
+            StopTimer();
+
+            timeRemain = Mathf.Max(0f, duration);
+            isRunning = true;
+            isPaused = false;
+        }
+
+        public void PauseTimer()
+        {
+            if (!isRunning) return;
+
+            isPaused = true;
+        }
+
+        public void StopTimer()
+        {
+            timeRemain = 0f;
+            isRunning = false;
+            isPaused = false;
+        }
+
+        public void Update(float deltaTime)
+        {
+            if (!isRunning || isPaused) return;
+
+            timeRemain -= deltaTime;
+
+            if (timeRemain > 0f) return;
+
+            timeRemain = 0f;
+            isRunning = false;
+            isPaused = false;
+
+            Debug.Log("Loss timer finished. Player loses.");
+        }
+    }
 }
