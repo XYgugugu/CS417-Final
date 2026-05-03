8bc070eaa5948506b6b28153400e2d8c66c9b1b6
Cindy4869 <2948292626@qq.com>
2026-05-03 02:52:07 -0500

    level transition script and prefab

--- Assets/PVZ/Core/MainMenuManager.cs
+++ Assets/PVZ/Core/MainMenuManager.cs
@@ -5,9 +5,18 @@ namespace PVZ3D.Core
 {
     public class MainMenuManager : MonoBehaviour
     {
+        [SerializeField] public string firstLevelSceneName = "Farm";
+
+        public void StartGame()
+        {
+            Debug.Log("Trying to load scene: " + firstLevelSceneName);
+            SceneManager.LoadScene(firstLevelSceneName);
+        }
+        
+        //override
         public void StartGame(string sceneName)
         {
-            Debug.Log("Trying to load scene: " + sceneName);        
+            Debug.Log("Trying to load scene: " + sceneName);
             SceneManager.LoadScene(sceneName);
         }
 
