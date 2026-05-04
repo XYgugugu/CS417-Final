f55c7fa21f27c68cf67c3a2d9b16ddfadeaa78b3
Jerry <2021957583xwf@gmail.com>
2026-05-04 17:49:34 -0500

    some final change

---
+++ Assets/PVZ/Core/tpportal.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class tpportal : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Camera portalCamera;
+    public GameObject thePlayer;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        thePlayer.transform.position += portalCamera.transform.position;
+        Debug.Log("teleport");
+    }
+}
